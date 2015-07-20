f28865ad90ce56de6b0978598e6a41f88c2f4933
Erwan Quélin <erwan.quelin@gmail.com>
2015-07-20 22:19:01 +0200

    Ajout script correction pb init.d manquant

--- add_initd_vmware_tools.sh
+++ add_initd_vmware_tools.sh
@@ -2,7 +2,7 @@
 
 cp /etc/vmware-tools/services.sh /etc/init.d/vmware-tools
 
-perl -0777 -i.original -pe 's/##VMWARE_INIT_INFO##/# Basic support for IRIX style chkconfig\n# chkconfig: 2345 90 60\n# description: Manages the services needed to run VMware software /igs' /etc/init.d/vmware-tools
+perl -0777 -i.original -pe 's/##VMWARE_INIT_INFO##/# chkconfig: 2345 90 60\n# description: Manages the services needed to run VMware Tools /igs' /etc/init.d/vmware-tools
 
 chkconfig --add vmware-tools
 chkconfig vmware-tools on
